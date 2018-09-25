66bafc11d7e3e818978802b5480fb1ba0a39ca1b
Gihan chanaka <gcbeligaswaththa@gmail.com>
2018-09-25 20:49:49 +0530

    Initial Commit

---
+++ OdocTestApp/Model/DoctorModel.swift
@@ -0,0 +1,9 @@
+//
+//  DoctorModel.swift
+//  OdocTestApp
+//
+//  Created by Gihan Chanaka Beligaswaththa on 9/25/18.
+//  Copyright © 2018 Gihan Chanaka Beligaswaththa. All rights reserved.
+//
+
+import Foundation
