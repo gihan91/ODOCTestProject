45d38078e65bf4c5915076f2461484a83dd18980
Gihan chanaka <gcbeligaswaththa@gmail.com>
2018-09-25 17:14:06 +0530

    Initial Commit

---
+++ OdocTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OdocTestApp
+//
+//  Created by Gihan Chanaka Beligaswaththa on 9/25/18.
+//  Copyright © 2018 Gihan Chanaka Beligaswaththa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
